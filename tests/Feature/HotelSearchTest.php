3dff5e5dab7bf20212135b3cf1fdbc79d7acfa56
akmal maxkamov <maxkamov48@gmail.com>
2023-09-02 23:48:34 +0500

    add: House paginate test

--- tests/Feature/HotelSearchTest.php
+++ tests/Feature/HotelSearchTest.php
@@ -9,9 +9,20 @@ use Tests\TestCase;
 
 class HotelSearchTest extends TestCase
 {
-    public function test_is_alive() {
+    public function test_is_alive()
+    {
         $response = $this->get('/api/hotels/search');
 
         $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_paginate_house()
+    {
+        $response = $this->get('/api/hotels/search');
+
+        $data = $response['data'];
+        $expectedCount = 10;
+
+        $this->assertCount($expectedCount, $data);
     }
 }
